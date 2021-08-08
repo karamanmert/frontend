aa79c7dde3ee5ee3bee6e4f86704f40d2a82a380
Mert Karaman <mertkrmn06@gmail.com>
2021-08-08 13:25:29 +0300

    after spring security and basic auth

--- src/api/todo/HelloWorldService.js
+++ src/api/todo/HelloWorldService.js
@@ -4,11 +4,25 @@ class HelloWorldService {
     executeHelloWorldService() {
         return axios.get("http://localhost:8080/hello-world")
     }
+
     executeHelloWorldBeanService() {
         return axios.get("http://localhost:8080/hello-world-bean")
     }
+
     executeHelloWorldPathVariableService(name) {
-        return axios.get(`http://localhost:8080/hello-world-path/${name}`)
+        // let username = "mert";
+        // let password = "123";
+        // let basicAuthHeader = 'Basic ' + window.btoa(username+":"+password) // this syntax is standard-> Basic mert:123 //sending basic auth header...
+        // console.log(basicAuthHeader)
+        // console.log(`http://localhost:8080/hello-world-path/${name}`)
+        return axios.get(`http://localhost:8080/hello-world-path/${name}`,{withCredentials:true}
+            // ,
+            // {
+            //     headers: {
+            //         "Authorization": basicAuthHeader  //YOUR_HEADER_VALUE
+            //     }
+            //}
+        )
     }
 }
 
--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,15 +1,16 @@
 import React, {Component} from 'react'
 import HelloWorldService from "../../api/todo/HelloWorldService";
+import {Link} from "react-router-dom";
 
 class WelcomeComponent extends Component {
 
     constructor(props) {
         super(props);
 
-
         this.state = {
             welcomeMessage: ""
         }
+
         this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this)
         this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this)
         this.handleError = this.handleError.bind(this)
@@ -20,19 +21,18 @@ class WelcomeComponent extends Component {
             <>
                 <h1>Welcome!</h1>
                 <div className="container">
-                    Welcome {this.props.match.params.name}
+                    Welcome {this.props.match.params.name}.
+                    You can manage your todos <Link to="/todos">here</Link>.
                 </div>
-
-
                 <div className="container">
-                    Click here to get a customized welcome message..
-                    <button onClick={this.retrieveWelcomeMessage} className="btn btn-success">Get Welcome
-                        Message</button>
+                    Click here to get a customized welcome message.
+                    <button onClick={this.retrieveWelcomeMessage}
+                            className="btn btn-success">Get Welcome Message</button>
                 </div>
-
                 <div className="container">
                     {this.state.welcomeMessage}
                 </div>
+
             </>
         )
     }
@@ -49,19 +49,28 @@ class WelcomeComponent extends Component {
         HelloWorldService.executeHelloWorldBeanService()
             .then(respond => this.handleSuccessfulResponse(respond))
          */
-
         HelloWorldService.executeHelloWorldPathVariableService(this.props.match.params.name)
-            .then(respond => this.handleSuccessfulResponse(respond))
+            .then(response => this.handleSuccessfulResponse(response))
             .catch(error => this.handleError(error))
     }
 
     handleSuccessfulResponse(response){
+        console.log(response)
         this.setState({welcomeMessage : response.data.message})  //ilk durumda string çağırınca response.data yeterli
         // artık bir JSON aldıgımız için data.message dememiz lazım.
     }
     handleError(error){
-        console.log(error.response.data.message) //backend tarafından gönderilen hata mesajının yakalanması..
-        this.setState({welcomeMessage: error.response.data.message})
+        console.log(error.response)
+        let errorMessage = '';
+
+        if (error.message){  //if there is a errormsg
+            errorMessage+=error.message
+        }
+        if (error.response && error.response.data){ //if anything in response data.
+            errorMessage+=error.response.data.message;
+        }
+
+        this.setState({welcomeMessage: errorMessage})
     }
 }
 
